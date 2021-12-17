a6ea3887c79fac6175e49a146785b7b5da4ea9ab
Alexander Tretyakov <tpetyakov@gmail.com>
2021-12-17 11:28:47 +0300

    feat: add translation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import './styles/index.scss';
 import requirements from '../requirements.md';
 import initHeaderMenu from './header-menu';
 import initPortfolio from './portfolio';
+import initTranslate from './translate';
 
 initHeaderMenu();
 initPortfolio();
+initTranslate();
 
 // console.log(requirements);
