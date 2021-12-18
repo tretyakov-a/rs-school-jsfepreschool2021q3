09f2da0127a5c7fbe6f3ca47db99d2624d0ae770
Alexander Tretyakov <tpetyakov@gmail.com>
2021-12-18 20:40:36 +0300

    fix: fixes for validator.w3.org

--- src/theme.js
+++ src/theme.js
@@ -4,18 +4,21 @@ const root = document.querySelector(':root');
 let currentTheme = 'dark';
 
 function handleThemeSwitcherClick(e) {
-  link.classList.remove(`theme-switcher__link_${currentTheme}`);
   currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  link.classList.add(`theme-switcher__link_${currentTheme}`);
   changeTheme(currentTheme);
 }
 
 function changeTheme(theme) {
+  console.log('changeTheme to', theme)
   currentTheme = theme;
   localStorage.setItem('theme', theme);
   const prevTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
   root.classList.remove(prevTheme);
   root.classList.add(currentTheme);
+
+  link.classList.remove(`theme-switcher__link_${prevTheme}`);
+  link.classList.add(`theme-switcher__link_${currentTheme}`);
 }
 
 export default function init() {
